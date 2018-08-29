1c52e4c2429ba2a9a75901552516fc6d3b7d2608
Nadia Lee <miss.nadia.lee@gmail.com>
2018-08-29 21:32:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-function Scooter () {
-
+function Scooter (year, color, model) {
+  this.year = year;
+  this.color = color;
+  this.model = model;
 };
 
-function Driver () {
-
+function Driver (name, age, experience) {
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
 };
 
-function PickupLocation () {
-
+function PickupLocation (address, city) {
+  this.address = address;
+  this.city = city;
 };
